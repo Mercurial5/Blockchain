25256cbcd4936b265e52add28ca9a19567631103
Mercurial5 <dias.nespayev@gmail.com>
2023-09-24 22:18:23 +0600

    feat: add merkle_root to block

--- src/core/block.py
+++ src/core/block.py
@@ -13,14 +13,15 @@ class Block:
     _transactions: list[Transaction]
     nonce: int = 0
     difficulty: int = 2
+    merkle_root: str = field(init=False)
     hash: str = field(init=False)
     timestamp: datetime = field(default_factory=datetime.now)
 
     def __post_init__(self):
         transactions_str = [str(transaction) for transaction in self.transactions]
-        merkle_root = MerkleTree.build_tree(transactions_str)
+        self.merkle_root = MerkleTree.build_tree(transactions_str)
 
-        self.hash = self.__calculate_hash(merkle_root)
+        self.hash = self.__calculate_hash()
 
     @property
     def length(self) -> int:
@@ -30,11 +31,11 @@ class Block:
     def transactions(self) -> list[Transaction]:
         return self._transactions
 
-    def __calculate_hash(self, merkle_root: str) -> str:
+    def __calculate_hash(self) -> str:
         new_hash = ''
         while new_hash[:self.difficulty] != '0' * self.difficulty:
             self.nonce += 1
-            new_hash = f'{self.index}{self.previous_hash}{self.timestamp}{self.transactions}{merkle_root}{self.nonce}'
+            new_hash = f'{self.index}{self.previous_hash}{self.timestamp}{self.transactions}{self.merkle_root}{self.nonce}'
             new_hash = hashlib.sha256(new_hash.encode()).hexdigest()
 
         return new_hash
