e5189eb1c064d37c9be1082e9472931c7e9e3aef
daukah <126101355+daukah@users.noreply.github.com>
2023-09-24 21:08:52 +0600

    Update blockchain.py

--- src/core/blockchain.py
+++ src/core/blockchain.py
@@ -1,3 +1,4 @@
+from config import AMOUNT_OF_TRANSACTIONS_PER_BLOCK
 from core.block import Block
 from core.transaction import Transaction
 from utils import get_logger
@@ -6,10 +7,10 @@ logger = get_logger(__name__)
 
 
 class Blockchain:
-    AMOUNT_OF_TRANSACTIONS_PER_BLOCK = 100
+    mem_pool: list[Transaction] = []
 
     def __init__(self):
-        genesis_block = Block(0, '', '0')
+        genesis_block = Block(0, '', [])
         self.chain: list[Block] = [genesis_block]
 
     @property
@@ -18,16 +19,13 @@ class Blockchain:
 
     def add_transaction(self, transaction: Transaction) -> None:
         logger.info(f'Adding new transaction - {transaction}')
+        if len(self.mem_pool) == AMOUNT_OF_TRANSACTIONS_PER_BLOCK:
+            logger.info('Mem pool is full, creating a block')
+            block = Block(len(self.chain), self.latest.hash, self.mem_pool.copy())
+            self.chain.append(block)
+            self.mem_pool.clear()
 
-        if self.latest.length == self.AMOUNT_OF_TRANSACTIONS_PER_BLOCK:
-            logger.info('Current block holds max number of transaction, creating new block')
-            self.__add_block()
-
-        self.latest.add_transaction(transaction)
-
-    def __add_block(self) -> None:
-        block = Block(len(self.chain), self.latest.hash)
-        self.chain.append(block)
+        self.mem_pool.append(transaction)
 
     def __str__(self) -> str:
         return f'<Blockchain length={len(self.chain)}>'
